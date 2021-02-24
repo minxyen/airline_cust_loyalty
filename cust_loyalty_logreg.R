0a6f2316aba0d595ff26969beae46fc937a18504
Minyen Hsieh <stevehsieh2797@gmail.com>
2021-02-24 11:26:51 -0600

    merged two csv data

--- cust_loyalty_logreg.R
+++ cust_loyalty_logreg.R
@@ -2,3 +2,9 @@ library(tidyverse)
 
 internal.data <- read_csv("./data_folder/internal_data.csv")
 survey.data <- read_csv("./data_folder/survey_data.csv")
+
+head(internal.data)
+head(survey.data)
+
+whole_data <- merge(internal.data, survey.data, by = "user_id")
+head(whole_data)
