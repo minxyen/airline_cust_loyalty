01759018ff16c6984f04c4170e382d238f2f37c0
Minyen Hsieh <stevehsieh2797@gmail.com>
2021-02-26 15:28:39 -0600

    Build first model - focus on marketing factors. Created ROC curve + AUC value

--- cust_loyalty_logreg.R
+++ cust_loyalty_logreg.R
@@ -43,5 +43,42 @@ ggplot(melt(cor(dummy_whole_data[,2:ncol(dummy_whole_data)])),
   theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1),
         axis.title = element_blank())
 
+# ------- Logistic Regression Model Building ...
+# ------- Marketing Factor, Internal Data
+
+mrkt_model <- glm(is_loyal ~ dm_message + dm_post + dm_email +              
+      credit_card_vendor + credit_card_bonus + 
+      tv_ad + youtube_ad_1 + youtube_ad_2 + youtube_ad_3,
+    data = whole_data, family = binomial(link="logit"))
+
+summary(mrkt_model)
+
+# positive influence (α=0.05) variables：
+# - dm_post, dm_email, credit_card_bonus3, credit_card_bonus4, tv_ad, youtube_ad_2
+# negative influence (α=0.05) variables：
+# - dm_message, youtube_ad_1
+
+predict_prob <- predict(mrkt_model, whole_data, type = "response")
+library("InformationValue")
+# Calculate cut-off probability which minimized mis-classification error
+opt_cutoff <- optimalCutoff(whole_data$is_loyal, predict_prob)[1]
+# create confusion matrix
+confusionMatrix(whole_data$is_loyal, predict_prob, threshold = opt_cutoff)
+
+# mis-classification error (%)
+misClassError(whole_data$is_loyal, predict_prob, threshold = opt_cutoff)
+# 0.219 -> quite high
+
+# ROC curve + AUC value
+library("plotROC")
+prediction_table <- data.frame(true_label = whole_data$is_loyal,
+                            predict_prob = predict_prob)
+roc_curve <- ggplot(prediction_table,
+       aes(d = true_label, m = predict_prob)) + geom_roc() + 
+  geom_roc() 
+
+roc_curve + style_roc() +
+  annotate("text", x = 0.75, y = 0.25, size = 5, 
+           label = paste("AUC=", round(calc_auc(roc_curve)$AUC, 3)))
 
 
